32f1c168451137fa15a04df7fa448d2cf4020ade
@EvaMalouskova18 <evamalouskova@gmail.com>
2024-01-17 11:01:54 +0100

    Přidejte soubory projektu.

---
+++ Properties/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Threading.Channels;
+
+namespace Properties
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car car = new Car("Porsche");
+
+            Console.WriteLine(car.Model);
+
+            Console.ReadKey();
+        }
+
+        class Car
+        {
+            //String model;
+
+            //public string Model
+            //{
+            //    get { return model; }
+            //    set { model = value; }
+            //}
+
+            public string Model
+            {
+                get;
+                set;
+            }
+
+            public Car(String model) 
+            {
+                this.Model = model;
+            }
+        }
+    }
+}
